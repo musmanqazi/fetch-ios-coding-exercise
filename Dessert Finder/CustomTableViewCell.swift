caedebb1cfa58fd46affb5b51795d063a4606468
musmanqazi <usmanqazi8@gmail.com>
2024-06-14 21:51:39 -0700

    Added limited API functionality for testing

---
+++ Dessert Finder/CustomTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CustomTableViewCell.swift
+//  Dessert Finder
+//
+//  Created by Usman Qazi on 6/14/24.
+//
+
+import UIKit
+
+class CustomTableViewCell : UITableViewCell {
+    
+    @IBOutlet weak var thumbnailImage : UIImageView!
+    @IBOutlet weak var nameOfDessert : UILabel!
+    @IBOutlet weak var numberOfIngredients : UILabel!
+    @IBOutlet weak var countryOfOrigin : UILabel!
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
